a7f4c9b6ef2915e92b334e728c04d2d650f52f36
TomaszLaboj <tomasz.laboj@gmail.com>
2025-04-02 20:41:27 +0100

    update

--- src/main/java/org/example/MusicSearch.java
+++ src/main/java/org/example/MusicSearch.java
@@ -5,69 +5,120 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MusicSearch {
-    Map<String, Artist> data = new HashMap<>();
+    static Map<String, Artist> data = new HashMap<>();
 
-    public Song getData(String artistName, String albumName, String songName) {
+    public static void main(String[] args) {
+        addData("Phil Collins", new Date(1950, 12, 1), "UK", "Face value", "45:10", "1980", "In the air tonight", "Phil Collins", "05:00");
+        System.out.println(getData("Phil Collins", "Face value", "In the air tonight"));
+    }
+
+    public static String getData(String artistName, String albumName, String songName) {
         Artist artist = data.get(artistName);
         Album album = artist.getAlbum(albumName);
+        Song song = album.getSong(songName);
+        return new MusicData(artist.artistName, artist.born, artist.country, album.albumTitle, album.length, album.length, song.songTitle, song.musicAndLyricsAuthor, song.trackLength).toString();
+    }
 
-        return album.getSong(songName);
+    public static void addData(String artistName, Date born, String country, String albumName, String length, String yearOfRelase, String songTitle, String musicAndLyricsAuthor, String trackLength) {
+        Artist artist = new Artist(artistName, born, country);
+        Album album = new Album(albumName, length, yearOfRelase);
+        Song song = new Song(songTitle, musicAndLyricsAuthor, trackLength);
+
+        album.addSong(songTitle, song);
+        artist.addAlbum(albumName, album);
+        data.put(artistName, artist);
     }
 
     private static class MusicData {
-        Artist artist;
-        Album album;
-        String song;
+        String artistName;
+        Date born;
+        String country;
+        String albumTitle;
+        String length;
+        String yearOfRelease;
+        String songTitle;
+        String musicAndLyricsAuthor;
+        String trackLength;
+
+        public MusicData(String artistName, Date born, String country, String albumTitle, String length, String yearOfRelease, String songTitle, String musicAndLyricsAuthor, String trackLength) {
+            this.artistName = artistName;
+            this.born = born;
+            this.country = country;
+            this.albumTitle = albumTitle;
+            this.length = length;
+            this.yearOfRelease = yearOfRelease;
+            this.songTitle = songTitle;
+            this.musicAndLyricsAuthor = musicAndLyricsAuthor;
+            this.trackLength = trackLength;
+        }
+
+        @Override
+        public String toString() {
+            return "MusicData{" +
+                    "artistName='" + artistName + '\'' +
+                    ", born=" + born +
+                    ", country='" + country + '\'' +
+                    ", albumTitle='" + albumTitle + '\'' +
+                    ", length='" + length + '\'' +
+                    ", yearOfRelease='" + yearOfRelease + '\'' +
+                    ", songTitle='" + songTitle + '\'' +
+                    ", musicAndLyricsAuthor='" + musicAndLyricsAuthor + '\'' +
+                    ", trackLength='" + trackLength + '\'' +
+                    '}';
+        }
     }
 
     private static class Artist {
-        String name;
+        String artistName;
         Date born;
         String country;
 
-        public Artist(String name, Date born, String country) {
-            this.name = name;
+        public Artist(String artistName, Date born, String country) {
+            this.artistName = artistName;
             this.born = born;
             this.country = country;
         }
 
-        HashMap<String, Album> albums;
+        HashMap<String, Album> albums = new HashMap<>();
 
         Album getAlbum(String name) {
             return albums.get(name);
         }
 
-        public void addAlbum(String name, int length, int releaseYear) {
-            albums.put(name, new Album(name, length, releaseYear));
+        public void addAlbum(String name, Album album) {
+            albums.put(name, album);
         }
     };
 
     private static class Album {
-        String name;
-        int length;
-        int releaseYear;
+        String albumTitle;
+        String length;
+        String releaseYear;
 
-        public Album(String name, int length, int releaseYear) {
-            this.name = name;
+        public Album(String albumTitle, String length, String releaseYear) {
+            this.albumTitle = albumTitle;
             this.length = length;
+            this.releaseYear = releaseYear;
         }
-        HashMap<String, Song> songs;
+        HashMap<String, Song> songs = new HashMap<>();
 
         Song getSong(String title) {
             return songs.get(title);
         };
 
-        public void addSong(String title, int trackLength) {
-            songs.put(title, new Song(title, trackLength));
+        public void addSong(String title, Song song) {
+            songs.put(title, song);
         }
     };
 
     public static class Song {
-        String title;
-        int trackLength;
+        String songTitle;
+        String musicAndLyricsAuthor;
+        String trackLength;
 
-        public Song(String title, int trackLength) {
-            this.title = title;
+        public Song(String songTitle, String musicAndLyricsAuthor, String trackLength) {
+            this.songTitle = songTitle;
+            this.musicAndLyricsAuthor = musicAndLyricsAuthor;
             this.trackLength = trackLength;
         }
     };
