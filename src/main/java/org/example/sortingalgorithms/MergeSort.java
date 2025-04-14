abb235909643c8a813375a4d0899ac04cb8b9d91
TomaszLaboj <tomasz.laboj@gmail.com>
2025-04-14 23:39:26 +0100

    refactor to use Queue

--- src/main/java/org/example/sortingalgorithms/MergeSort.java
+++ src/main/java/org/example/sortingalgorithms/MergeSort.java
@@ -3,44 +3,46 @@ package org.example.sortingalgorithms;
 import org.example.MyQueue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] list = {1,6,34,9,0,2,8,23,23,55,1,765};
-        List<MyQueue<Integer>> splittedList = split(list);
-        List<MyQueue<Integer>> sorted = sort(splittedList);
+        int[] list = {1, 6, 34, 9, 0, 2, 8, 23, 23, -15, 1234, -99, 55, 1, 765};
+        List<Queue<Integer>> splittedList = split(list);
+        List<Queue<Integer>> sorted = sort(splittedList);
         int[] sortedList = convert(sorted);
         for (int i : sortedList) {
             System.out.println(i);
         }
     }
 
-    static List<MyQueue<Integer>> split(int[] list) {
-        List<MyQueue<Integer>> listToSort = new ArrayList<>();
+    static List<Queue<Integer>> split(int[] list) {
+        List<Queue<Integer>> listToSort = new ArrayList<>();
         for (int i = 0; i < list.length; i++) {
-            MyQueue<Integer> data = new MyQueue<>();
-            data.enqueue(list[i]);
+            Queue<Integer> data = new LinkedList<>();
+            data.offer(list[i]);
             listToSort.add(data);
         }
         return listToSort;
     }
 
-    static int[] convert(List<MyQueue<Integer>> list) {
+    static int[] convert(List<Queue<Integer>> list) {
         int size = list.get(0).size();
-        MyQueue<Integer> queue = list.remove(0);
+        Queue<Integer> queue = list.remove(0);
         int[] returnList = new int[size];
         for (int i = 0; i < size; i++) {
             try {
-                returnList[i] = queue.dequeue();
+                returnList[i] = queue.poll();
             } catch (Exception e) {};
         }
         return returnList;
     }
 
-    static List<MyQueue<Integer>> sort(List<MyQueue<Integer>> listToSort) {
+    static List<Queue<Integer>> sort(List<Queue<Integer>> listToSort) {
 
-        List<MyQueue<Integer>> sorted = new ArrayList<>();
+        List<Queue<Integer>> sorted = new ArrayList<>();
 
         int numberOfPairs;
         if (listToSort.size() % 2 == 0) {
@@ -50,42 +52,42 @@ public class MergeSort {
         }
 
         for (int i = 0, index = 0; i < numberOfPairs; i++, index = index + 2) {
-            MyQueue<Integer> sortedQueue = new MyQueue<>();
+            Queue<Integer> sortedQueue = new LinkedList<>();
 
             if (index + 1 == listToSort.size()) {
                 try {
-                    MyQueue<Integer> queue = listToSort.get(index);
+                    Queue<Integer> queue = listToSort.get(index);
 
                     sortedQueue = queue;
                 } catch (Exception e) {}
             } else {
-                MyQueue<Integer> left = listToSort.get(index);
-                MyQueue<Integer> right = listToSort.get(index + 1);
+                Queue<Integer> left = listToSort.get(index);
+                Queue<Integer> right = listToSort.get(index + 1);
                 while (left.size() > 0 && right.size() > 0) {
                     try {
                         Integer leftNumber = (Integer) left.peek();
                         Integer rightNumber = (Integer) right.peek();
                         if (leftNumber <= rightNumber) {
-                            left.dequeue();
-                            sortedQueue.enqueue(leftNumber);
+                            left.poll();
+                            sortedQueue.offer(leftNumber);
                         } else {
-                            right.dequeue();
-                            sortedQueue.enqueue(rightNumber);
+                            right.poll();
+                            sortedQueue.offer(rightNumber);
                         }
                     } catch (Exception e) {};
                 }
                 if (left.size() == 0 && right.size() != 0) {
                     while (right.size() > 0) {
                         try {
-                            Integer number = right.dequeue();
-                            sortedQueue.enqueue(number);
+                            Integer number = right.poll();
+                            sortedQueue.offer(number);
                         } catch (Exception e) {};
                     }
                 } else if (left.size() != 0 && right.size() == 0) {
                     while (left.size() > 0) {
                         try {
-                            Integer number = left.dequeue();
-                            sortedQueue.enqueue(number);
+                            Integer number = left.poll();
+                            sortedQueue.offer(number);
                         } catch (Exception e) {}
                     }
                 }
@@ -94,9 +96,7 @@ public class MergeSort {
             sorted.add(i, sortedQueue);
 
         }
-        for (MyQueue<Integer> q : sorted) {
-            System.out.println(q.toString());
-        }
+
         if (sorted.size() > 1) {
             return sort(sorted);
         } else {
