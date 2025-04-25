4d76ab1de6b9929fc7781ef20f93b12b995d784b
TomaszLaboj <tomasz.laboj@gmail.com>
2025-04-25 17:19:45 +0100

    add check letters

--- src/main/java/org/example/Boggle.java
+++ src/main/java/org/example/Boggle.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Boggle {
     static private String[][] grid = {
             {"A", "T", "E", "E"},
@@ -10,23 +14,37 @@ public class Boggle {
 
     static private String[] dictionary;
 
-    static private String[] foundWords;
 
     public static void main(String[] args) {
         System.out.println("boggle");
     }
 
-    static String[] findWords(String[][] letterGrid){
+    static List<String> findWords(String[][] letterGrid){
+        List<String> foundWords = new ArrayList<>();
         for (String word : dictionary) {
             String[] splitWord = word.split("");
             for (int row = 0; row < grid.length; row++) {
                 for (int col = 0; col < grid[0].length; col++) {
                     if (splitWord[0].equals(grid[row][col])) {
-                        
+
+                        if(checkLetters(row, col, letter)) {
+                            foundWords.add(word);
+                        }
                     }
                 }
             }
         }
+        return foundWords;
+    };
+
+    static boolean checkLetters(int row, int col,char letter) {
+        for (int newRow = row - 1; newRow < 2; newRow++) {
+            for (int newCol = col - 1; newCol < 2; newCol++) {
+                if(grid[newRow][newCol].equals(letter));
+                return true;
+            }
+        }
+        return false;
     };
 }
 
@@ -40,7 +58,10 @@ public class Boggle {
         recursively
         if yes, take the next letter and check if it's present in the surrounding of the previous letter
             check if out of bounds
-            don't check position of the previous letter
+            don't check position of the previous letter as the same letter cannot be reused in a single word
   }
-
+    version2;
+    brute force
+    create words from the grid, 2 words first , then 3 ,
+    then compare all the new words with the dictionary
  */
