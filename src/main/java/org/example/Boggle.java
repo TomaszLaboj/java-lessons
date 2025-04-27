1ae237bb58e170143f846241f6f0fa1fd342d5cd
TomaszLaboj <tomasz.laboj@gmail.com>
2025-04-27 19:44:07 +0100

    boggle finished probably

--- src/main/java/org/example/Boggle.java
+++ src/main/java/org/example/Boggle.java
@@ -7,14 +7,13 @@ import java.util.stream.Collectors;
 
 public class Boggle {
     static private String[][] grid = {
-            {"A", "A", "A", "A"},
+            {"P", "A", "A", "A"},
             {"A", "P", "A", "A"},
-            {"A", "A", "A", "A"},
+            {"T", "A", "A", "A"},
             {"A", "A", "A", "T"}
     };
 
-    static  String[] listOfWords = {"PAT", "TYPE", "TEPID", "HOUSE", "APPLE", "USE", "PET", "PINS", "GOAT", "JAVA", "NOT", "TAP"};
-
+    static  String[] listOfWords = {"PAT", "TAP"};
 
     public static void main(String[] args) {
         List<String> foundWords = findWords(grid);
@@ -23,20 +22,66 @@ public class Boggle {
 
     static List<String> findWords(String[][] letterGrid){
         List<String> foundWords = new ArrayList<>();
+        Queue<Point> firstLetterCoordinates;
         for (String word : listOfWords) {
+            firstLetterCoordinates = new LinkedList<>();
             Queue<String> splitWordQueue = new LinkedList<>(Arrays.asList(word.split("")));
             String firstLetter = splitWordQueue.poll();
             for (int row = 0; row < grid.length; row++) {
                 for (int col = 0; col < grid[0].length; col++) {
                     if (firstLetter.equals(grid[row][col])) {
-                        if(checkLetters(row, col, splitWordQueue, letterGrid, row, col)) {
-                            foundWords.add(word);
-                        }
+                        firstLetterCoordinates.offer(new Point(row, col));
                     }
+                }
+            }
+            for (Point p : firstLetterCoordinates) {
+                if(checkSurroundingLetters(p, splitWordQueue, letterGrid, firstLetterCoordinates.peek())) {
+                    foundWords.add(word);
                 }
             }
         }
         return foundWords;
+    };
+
+    static private boolean checkSurroundingLetters(Point firstLetterCoordinate, Queue<String> word, String[][] letterGrid, Point previousLetterCoordinate) {
+        Queue<String> wordCopy = new LinkedList<>(word);
+        String letterToSearch = wordCopy.poll();
+        boolean letterFound = false;
+        boolean wordFound = false;
+        List<Point> coordinatesWithFoundLetters = new ArrayList<>();
+
+        List<Point> coordinates = calculateSurroundingCoordinates(firstLetterCoordinate.x, firstLetterCoordinate.y);
+        List<Point> coordinatesToCheck = coordinates
+                .stream()
+                .filter(point -> point.x >= 0)
+                .filter(point -> point.x < letterGrid.length)
+                .filter(point -> point.y >= 0)
+                .filter(point -> point.y < letterGrid[0].length)
+                .filter(point -> pointIsNotPreviousPoint(point.x, point.y, previousLetterCoordinate.x, previousLetterCoordinate.y))
+                .collect(Collectors.toList());
+
+        for (Point p2 : coordinatesToCheck) {
+            if (wordCopy.size() == 0 && letterGrid[p2.x][p2.y].equals(letterToSearch)) {
+                wordFound = true;
+            }
+            if (wordCopy.size() != 0 && letterGrid[p2.x][p2.y].equals(letterToSearch)) {
+                letterFound = true;
+                coordinatesWithFoundLetters.add(new Point(p2.x, p2.y));
+            }
+        }
+
+        if (wordFound) {
+            return true;
+        }
+
+        if (letterFound) {
+            for (Point p3 : coordinatesWithFoundLetters) {
+                if (checkSurroundingLetters(p3, wordCopy, letterGrid, firstLetterCoordinate)) {
+                    return checkSurroundingLetters(p3, wordCopy, letterGrid, firstLetterCoordinate);
+                }
+            }
+        }
+        return false;
     };
 
     static boolean pointIsNotPreviousPoint(int x, int y, int prevX, int prevY) {
@@ -48,46 +93,7 @@ public class Boggle {
         }
         return flag;
     }
-
-    static private boolean checkLetters(List<Point> allCoordinatesWithLetters,Queue<String> word, String[][] letterGrid, int prevRow, int prevCol) {
-        Queue<String> wordCopy = new LinkedList<>(word);
-        String letter = wordCopy.poll();
-        boolean letterFound = false;
-        boolean wordFound = false;
-        List<Point> coordinatesWithFoundLetters = new ArrayList<>();
-        Point examinedPoint = new Point();
-
-        for (Point p1 : allCoordinatesWithLetters) {
-            examinedPoint.x = p1.x;
-            examinedPoint.y = p1.y;
-            List<Point> coordinates = calculateCoordinates(p1.x, p1.y);
-            List<Point> coordinatesToCheck = coordinates
-                    .stream()
-                    .filter(point -> point.x >= 0)
-                    .filter(point -> point.x < letterGrid.length)
-                    .filter(point -> point.y >= 0)
-                    .filter(point -> point.y < letterGrid[0].length)
-                    .filter(point -> pointIsNotPreviousPoint(point.x, point.y, prevRow, prevCol))
-                    .collect(Collectors.toList());
-
-            for (Point p2 : coordinatesToCheck) {
-                if (wordCopy.size() == 0 && letterGrid[p2.x][p2.y].equals(letter)) {
-                    wordFound = true;
-                }
-                if (wordCopy.size() != 0 && letterGrid[p2.x][p2.y].equals(letter)) {
-                    letterFound = true;
-                    coordinatesWithFoundLetters.add(new Point(p2.x, p2.y));
-                }
-            }
-        }
-
-        if (letterFound) {
-            return checkLetters(coordinatesWithFoundLetters, wordCopy, letterGrid, examinedPoint.x, examinedPoint.y);
-        }
-        return wordFound;
-    };
-
-    static private List<Point> calculateCoordinates(int row, int col) {
+    static private List<Point> calculateSurroundingCoordinates(int row, int col) {
         List<Point> result = new ArrayList<>();
         result.add(new Point(row, col + 1));
         result.add(new Point(row + 1, col + 1));
@@ -105,7 +111,7 @@ public class Boggle {
 
 /*
   pseudocode
-  version 1:
+  version 1 (above):
   LOOP OVER THE WORDS FROM DICTIONARY {
       1. Take a word from the dictionary
       2. check if the 1st letter is in the grid
